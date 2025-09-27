dd8ad007909ca819e86dd1a3d0542cd7b9abded2
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2025-09-27 12:08:12 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Targeting Elements
+
+let heading = document.getElementById('heading');
+let para = document.querySelector('.para');
+let items = document.querySelectorAll('li');
+
+// Manipulating Content
+
+heading.textContent = 'Typescript DOM Assignment';
+para.innerHTML = 'InshaAllah Future MERN Stack Developer and AI Architect';
+
+// Class and Style
+
+heading.classList.add('1');
+para.style.color = 'red';
+
